5790002f1cef2a6cf51081794ba942ffa6cd4097
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2025-06-20 14:16:51 -0400

    New current time
    Add chrono dependency and display current time in main function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use chrono::Local;
 fn main() {
     println!("Hello, world!");
     for i in 1..=5 {
         println!("Number: {}", i);
     }
+    println!("Current time: {}", Local::now().format("%Y-%m-%d %H:%M:%S"));
     println!("Goodbye, world from Wilhelm!");
 }
